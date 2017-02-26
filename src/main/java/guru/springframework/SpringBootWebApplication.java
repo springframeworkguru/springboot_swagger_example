40f45383faa049647e3e36c1f8899fa83c2acea6
Ximanta <ximaanta.sarma@gmail.com>
2017-02-26 21:54:36 +0530

    Added ApiInfo

--- src/main/java/guru/springframework/SpringBootWebApplication.java
+++ src/main/java/guru/springframework/SpringBootWebApplication.java
@@ -2,6 +2,7 @@ package guru.springframework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
--- src/main/java/guru/springframework/domain/Product.java
+++ src/main/java/guru/springframework/domain/Product.java
@@ -1,5 +1,7 @@
 package guru.springframework.domain;
 
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.persistence.*;
 import java.math.BigDecimal;
 
@@ -7,14 +9,18 @@ import java.math.BigDecimal;
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @ApiModelProperty(notes = "The database generated product ID")
     private Integer id;
-
     @Version
+    @ApiModelProperty(notes = "The auto-generated version of the product")
     private Integer version;
-
+    @ApiModelProperty(notes = "The application-specific product ID")
     private String productId;
+    @ApiModelProperty(notes = "The product description")
     private String description;
+    @ApiModelProperty(notes = "The image URL of the product")
     private String imageUrl;
+    @ApiModelProperty(notes = "The price of the product", required = true)
     private BigDecimal price;
 
     public String getDescription() {
